07a478833acdaa3187416f5eaf949e272e50adcb
afeniaguilar <league@FunkyMini.local>
2017-03-14 18:29:14 -0700

    day one i did a code for the robot how the robot go, tern, and draw

---
+++ src/HelloWorld.java
@@ -0,0 +1,93 @@
+import org.jointheleague.graphical.robot.Robot;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		Robot dagim =new Robot("batman");
+    dagim.setSpeed(10);
+	dagim.penDown();
+for (int i = 0; i < 8; i++) {
+	dagim.move(100);
+	dagim.turn(90);
+	dagim.move(100);
+	dagim.turn(30);
+	dagim.move(100);
+	dagim.turn(20);	
+	
+	
+	
+	
+	
+}		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+	
+	
+	
+	
+	
+	}
+}
